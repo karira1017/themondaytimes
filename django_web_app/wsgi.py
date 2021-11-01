0aeefab3ae6384326640c81f7474e38872b55986
karira1017 <kimatakarira@gmail.com>
2021-11-01 04:17:02 +0300

    'commit'

--- django_web_app/wsgi.py
+++ django_web_app/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+from pathlib import Path
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_web_app.settings')
